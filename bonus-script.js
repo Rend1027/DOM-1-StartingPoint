2e11c37e3f9ea366a97c366474fd8fe86c2cb941
Elian Echavarria <elianstanly431@gmail.com>
2025-06-18 16:08:47 -0400

    Final Bonus Commit

--- bonus-script.js
+++ bonus-script.js
@@ -2,7 +2,36 @@ console.log("hello from JS");
 
 const root = document.getElementById("root");
 const h1 = document.createElement("h1");
+const h2 = document.createElement("h2");
+const ul = document.createElement("ul");
+const p = document.createElement("p");
+const li = document.createElement("li");
 h1.innerText =
   "Hello! This is my HTML website created entirely from JavaScript";
+h2.innerText = "Ok, we see your message? So what now!";
+p.innerText = "Possible things to do:";
 h1.style.fontFamily = "Georgia, serif";
 root.appendChild(h1);
+root.appendChild(h2);
+root.appendChild(p);
+root.appendChild(ul);
+
+let num = 3;
+
+for (let i = 0; i < num; i++) {
+  const newli = document.createElement('li');
+  console.log(root.appendChild(newli));
+  newli.classList.add(`bullet-${i}`);
+}
+
+
+let liTextContent = ["Make a header", "Make a ul", "Make a footer"];
+
+for (let i = 0; i < liTextContent.length; i++) {
+  const listItem = document.querySelectorAll(`.bullet-${i}`);
+  listItem[0].textContent = `${liTextContent[i]}`;
+}
+
+
+
+
